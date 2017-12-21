21815ac9ce6fa8829b0b9789edc979e6514511a8
joshua-t-loong <joshua.t.loong@gmail.com>
2017-12-21 17:09:29 -0500

    Fixed merging and downloading from SQLite

--- app.py
+++ app.py
@@ -1,53 +1,61 @@
-import os, errno, glob, time
-from flask import Flask, request, redirect, url_for, send_file, render_template
+import os, errno, glob, time, random, string, io
+from flask import Flask, request, redirect, url_for, send_file, render_template, make_response
 from werkzeug import secure_filename
 from PyPDF2 import PdfFileMerger
+from datetime import datetime
 import sqlite3 as sql
 
-UPLOAD_FOLDER = './uploads'
+
 ALLOWED_EXTENSIONS = set(['pdf'])
-userIP = ""
-timeOfVisit = ""
 
 app = Flask(__name__)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
 
 def allowed_file(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1] in ALLOWED_EXTENSIONS
 
+def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+    return ''.join(random.choice(chars) for _ in range(size))
+
 
 @app.route("/", methods=['GET', 'POST'])
 def index():
-    try:
-        os.makedirs("./downloads")
-    except OSError as e:
-        if e.errno != errno.EEXIST:
-            raise
+    user = id_generator()
 
-    print(userIP + " - " + timeOfVisit)
-    return render_template('index.html', title='Home')
+    resp = make_response(render_template('index.html', title='Home'))
+    resp.set_cookie('userID', user)
+    print(user)
+
+    return resp
 
 @app.route("/upload/", methods=['POST'])
 def upload():
     if request.method == 'POST':
         try:
-            userIP = str(request.remote_addr)
+            print("Hello?")
+            userID = str(request.cookies.get('userID'))
+            print(userID)
+            time = str(datetime.now())
+            print(time)
             f = request.files['file']
-            print(f.filename)
+
             con = sql.connect("database.db")
-            print(con)
             cur = con.cursor()
+
+            print(f.filename)
+            print(con)
             print(cur)
-            print("User: " + userIP + " | time of visit: " + str(time.asctime(time.localtime(time.time()))) + "| file:" + f.filename)
-            cur.execute("INSERT INTO files (user, timeOfVisit, filename,file) VALUES (?,?,?,?)",(userIP,timeOfVisit, f.filename,f.read()))
+            print("User: " + userID + " | time of visit: " + time + "| file:" + f.filename)
+
+            cur.execute("INSERT INTO files (user, timeOfVisit, filename,file) VALUES (?,?,?,?)",(userID,time, f.filename,f.read()))
             con.commit()
             print("Record successfully added")
+
             cur.close()
             con.close()
-        except Exception as e:
-            print("error in insert operation: " + e)
+        except:
+            print("error in insert operation")
             return render_template('index.html', title='Home')
         finally:
             return render_template('index.html', title='Home')
@@ -59,31 +67,21 @@ def upload():
 def merge():
     con = sql.connect("database.db")
     cur = con.cursor()
-    data = cur.execute("SELECT * FROM files WHERE user =? AND timeOfVisit=?",(userIP))
+    user = str(request.cookies.get('userID'))
+    data = cur.execute("SELECT * FROM files WHERE (user =?)",(user,))
     pdfs = []
-    # while True:
-    #
-    #     rows = cur.fetchall()
-    #     for row in rows:
-    #         if row == None:
-    #             break
-    #         print (row[2])
-    #
+    merger = PdfFileMerger()
+    for row in data:
+        print("User: " + row[0] + " | time of visit: " + row[1] + "| file:" + row[2])
+        merger.append(io.BytesIO(row[3]))
 
-    # print(pdfs)
-    # merger = PdfFileMerger()
-    # for pdf in pdfs:
-    #     merger.append(pdf)
-    # with open('./uploads/result.pdf', 'wb') as fout:
-    #     merger.write(fout)
-    return render_template('index.html', title='Home')
-
-@app.route("/download/", methods=['GET'])
-def download():
+    with open('./downloads/result.pdf', 'wb') as fout:
+        merger.write(fout)
     return send_file('downloads/result.pdf',
-                     mimetype='application/pdf',
-                     attachment_filename='result.pdf',
-                     as_attachment=True)
+                         mimetype='application/pdf',
+                         attachment_filename='result.pdf',
+                         as_attachment=True)
+
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=5001, debug=True)
